4180b16e140f4c3dccd2c292f5fa5852e235bea4
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-06-30 13:02:27 +1000

    completed

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+    return a+b
+}
+add(3,3)
+function subtract(a,b){
+    return a-b
+}
+subtract(12,3)
+function multiply(a,b){
+    return a*b
+}
+multiply(12,3)
+function divide(a,b){
+    return a/b
+}
+divide(12,3)
+function increment(n){
+    return n+=1
+}
+increment(5)
+function decrement(n){
+return n-=1
+}
+decrement(5)
+function makeInt(n){
+    return parseInt(n)
+}
+makeInt('10')
+function preserveDecimal(n)
+{
+return n
+}
+preserveDecimal('2.222')
